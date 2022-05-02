7df9fdac4682cbe765e27c6817fcdc1738d112de
Crowdbotics <team@crowdbotics.com>
2022-05-02 03:22:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "just_wallpapers_35039",
+  url: "https://just_wallpapers_35039.botics.co",
+  api: "https://just_wallpapers_35039.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
